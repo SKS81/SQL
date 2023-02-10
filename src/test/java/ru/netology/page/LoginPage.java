4d8974054bd3d4dca565c709c581af1e88415a9b
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2023-02-10 23:47:09 +0300

    Add page/LoginPage.java file

---
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -0,0 +1,13 @@
+package ru.netology.page;
+
+import ru.netology.data.User;
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+    public VerificationPage validLogin(User info) {
+        $("[data-test-id=login] input").setValue(info.getLogin());
+        $("[data-test-id=password] input").setValue(info.getPassword());
+        $("[data-test-id=action-login]").click();
+        return new VerificationPage();
+    }
+}
